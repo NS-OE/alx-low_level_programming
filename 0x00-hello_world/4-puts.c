833dc0ca24c6e6133473a87b0f690de25adc6253
NS-OE <sonezokoliejike+github@gmail.com>
2022-09-08 16:30:23 -0700

    C Tasks 1-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int  main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int  main(void)
 {
-  char str[]= "\"Programming is like building a multilingual puzzle";
-  puts(str);
-  return (0);
+char str[] = "\"Programming is like building a multilingual puzzle";
+
+puts(str);
+return (0);
 }
