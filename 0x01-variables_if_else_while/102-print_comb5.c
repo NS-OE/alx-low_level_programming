28185fa24bcfa3ed7c8f0b94dcdeab7624d4bb6a
NS-OE <sonezokoliejike+github@gmail.com>
2022-09-12 14:33:51 -0700

    Advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,35 +2,33 @@
 /**
  * main - prints all possible combination of 2 digits b/w 0
  * and 99, seperated by space and joined with comma.
-*
-* Return: Always 0 (Success)
-*/
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int plus, pl, prus, pr;
+int plus, pl;
 
-for (plus = 0 ; plus < 10 ; plus++)
+for (plus = 0 ; plus < 100 ; plus++)
 {
-for (pl = 0 ; pl < 10 ; pl++)
+for (pl = 0 ; pl < 100 ; pl++)
 {
-for (prus = 0 ; prus < 10 ; ++prus)
+if (plus < pl)
 {
-for (pr = 0 ; pr < 10 ; ++pr)
-{
-putchar(plus + '0');
-putchar(pl + '0');
+putchar((plus / 10) + '0');
+putchar((plus % 10) + '0');
 putchar(' ');
-putchar(prus + '0');
-putchar(pr + '0');
-if (pr <= 9)
+putchar((pl / 10) + '0');
+putchar((pl % 10) + '0');
+}
+if (plus != 98 || pl != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
-}
-}
+
 putchar('\n');
 return (0);
 }
