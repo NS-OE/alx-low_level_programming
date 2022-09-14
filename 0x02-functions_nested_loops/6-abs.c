58234da98ce2cb27073d3759cf9321e50bfa74dd
NS-OE <sonezokoliejike+github@gmail.com>
2022-09-13 17:46:00 -0700

    Tasks 5-8&10

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs  - print absolute value
+ *
+ * @int: data type
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int _abs(int)
+{
+
+int n, nabs;
+
+nabs = abs(n);
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit  - printsblast digit of a no
+ *
+ * @int: data type
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int print_last_digit(int)
+{
+
+int n, l_dg;
+
+l_dg = n % 10;
+
+return (0);
+}
