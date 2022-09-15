6789a828551fa53c9b13694bb56561fa1f80aee0
NS-OE <sonezokoliejike+github@gmail.com>
2022-09-15 12:54:01 -0700

    Debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
 #include "main.h"
-/**
- * positive_or_negative - Entry point
- *
- * @i: integer variable
- *
- * Return: Always 0 (Success)
- */
-void positive_or_negative(int i)
-{
 
-if (i > 0)
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
 {
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}
-return;
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
