2251ec7b4bc21a6acb63c1e8433b6dbe5a72fd29
NS-OE <sonezokoliejike+github@gmail.com>
2022-09-24 09:55:19 -0700

    ADV

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,43 +9,43 @@
 
 void print_times_table(int n)
 {
-	int i, p, r;
+int i, p, r;
 
-	if (!(n > 15 || n < 0))
+if (!(n > 15 || n < 0))
+{
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (p = 0; p <= n; p++)
 		{
-			for (p = 0; p <= n; p++)
+		r = i * p;
+		if (r < 10)
+		{
+			if (!(p == 0 && r == 0))
 			{
-				r = i * p;
-				if (r < 10)
-				{
-					if (!(p == 0 && r == 0))
-					{
-						putchar(' ');
-						putchar(' ');
-						putchar(' ');
-					}
-					putchar(r + '0');
-				}
-				else if (r > 9 && r < 100)
-				{
-					putchar(' ');
-					putchar(' ');
-					putchar((r / 10) + '0');
-					putchar((r % 10) + '0');
-				{
-					else
-					{
-						putchar(' ');
-						putchar((r / 100) + '0');
-						putchar(((r / 10) % 10) + '0');
-						putchar((r % 10) + '0');
-					}
-					if (p != n)
-						putchar(',');
-				}
-				putchar('\n');
-				}
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
 			}
+			_putchar(r + '0');
 		}
+		else if (r > 9 && r < 100)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar((r / 100) + '0');
+		_putchar(((r / 10) % 10) + '0');
+		_putchar((r % 10) + '0');
+		}
+		if (p != n)
+		_putchar(',');
+		}
+	_putchar('\n');
+	}
+}
+}
