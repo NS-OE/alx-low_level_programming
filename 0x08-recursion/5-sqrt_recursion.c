00cc78f214119a986dfc3308aec49a2131bd12ae
NS-OE <sonezokoliejike+github@gmail.com>
2022-09-28 21:37:38 -0700

    Task 3-6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,37 @@
 #include "main.h"
-#include <math.h>
+
+
+int _sqrt_guess(int sq, int n);
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  *
+ * @n:  Sqr root variable
  * @n:  Sqr root variable
  * Return: returns square root or -1 (for error) to main.
  */
 
 int _sqrt_recursion(int n)
 {
-	if ((n < 0) || (n % 10 != 7))
+	if (n < 0)
 		return (-1);
 
-	return (sqrt(n));
+	return (_sqrt_guess(1, n));
+}
+
+/**
+ * _sqrt_guess - returns a guess on what the square of the square root no is.
+ *
+ * @sq:  Sqr variable
+ * @n:  Sqr root variable
+ * Return: returns square root or -1 (for error) to main.
+ */
+
+int _sqrt_guess(int sq, int n)
+{
+	if (n == sq * sq)
+		return (sq);
+	if (n < sq * sq)
+		return (-1);
+
+	return (_sqrt_guess(sq + 1, n));
 }
