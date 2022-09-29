6b5c0db7fec6c005fd93beee6fe94fe0b10d05d1
NS-OE <sonezokoliejike+github@gmail.com>
2022-09-28 20:20:28 -0700

    Tasks 3-6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,10 +9,10 @@
 
 int is_prime_number(int n)
 {
-	if ((n == 2) || (n == 3) || (n == 5) || (n == 7))
-		return (1);
-	if (!((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0) || (n % 7 == 0)) && (n >= 2))
-		return (1);
-	else
-		return (0);
+if ((n == 2) || (n == 3) || (n == 5) || (n == 7))
+	return (1);
+if (((n % 2 != 0) || (n % 3 != 0) || (n % 5 != 0) || (n % 7 != 0)) && (n >= 2))
+	return (1);
+else
+	return (0);
 }
