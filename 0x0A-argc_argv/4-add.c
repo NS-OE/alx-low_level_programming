c28c079c4414862e60d6367bcb66134dc319ef7a
NS-OE <sonezokoliejike+github@gmail.com>
2022-10-01 20:04:01 -0700

    Args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,17 +12,22 @@ int main(int argc, char *argv[])
 {
 int i, sum;
 
-for (i = 0; i < argc; i++)
-	if (!((atoi(argv[i]) % 10) >= 0 && (atoi(argv[i]) % 10) <= 9))
+if (argc > 2)
+{
+for (i = 1; i < argc; i++)
+	if (atoi(argv[i]) != 0)
+	{
+		sum += atoi(argv[i]);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		sum += atoi(argv[i]);
-	}
 printf("%d\n", sum);
+}
+else
+	printf("0\n");
 
-	return (0);
+return (0);
 }
