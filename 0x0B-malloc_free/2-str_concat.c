93fb9958197c5627f04a690b5e0e8fd11e39c52a
NS-OE <sonezokoliejike+github@gmail.com>
2022-10-04 21:58:05 -0700

    Malloc 0-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ unsigned int i, len1, len2, lenT;
 
 	if (s1 == NULL)
 	{
-		*s1 = "";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		*s2 = "";
+		s2 = "";
 	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
