8a0472072bcca166fb7371dcf6c1f32a46d3462c
NS-OE <sonezokoliejike+github@gmail.com>
2022-10-10 19:57:07 -0700

    doggo

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,27 @@ void print_dog(struct dog *d)
 {
 
 	if (d == NULL)
+	{
 		return;
+	}
 	if (d->name == NULL)
+	{
 		d->name = "nil";
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
+	{
 		d->owner = "nil";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,7 +9,13 @@
 
 void free_dog(dog_t *d)
 {
-free(d);
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);	
+	free(d);
 }
 
 
