8d5e8aa4cf312d38951996f488a44ccf90369915
NS-OE <sonezokoliejike+github@gmail.com>
2022-10-10 19:27:11 -0700

    doggo

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,24 +6,18 @@
  *
  * @d: pointer to address of strut
  */
-int main(void)
-{
-  /*  struct dog my_dog;
 
-  my_dog.name = NULL;
-  my_dog.age = 0;
-  my_dog.owner = NULL;*/
-  print_dog(NULL);
-  return (0);
-}
 void print_dog(struct dog *d)
 {
 
 	if (d == NULL)
 		return;
+	else
+	{
 	if (d->name == NULL)
 		d->name = "nil";
 	if (d->owner == NULL)
 		d->owner = "nil";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
 }
