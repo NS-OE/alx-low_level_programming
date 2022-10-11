7abad1547379ae29e559f2adf79ded6aba7ccf75
NS-OE <sonezokoliejike+github@gmail.com>
2022-10-11 05:15:02 -0700

    doggo

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner == NULL)
 	{
-		d->owner = "nil";
+		d->owner = "(nil)";
 	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
