f5f6c59ea18601757d968f8a26077f98efba193e
NS-OE <sonezokoliejike+github@gmail.com>
2022-10-30 17:51:42 -0700

    Debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,10 +12,14 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-  	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
+	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
 	{
-	  /*		printf("Day of the year: %d\n", day);
-	 	printf("Remaining days: %d\n", 366 - day);*/
+		if (month > 2)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -25,8 +29,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-		  /*			printf("Day of the year: %d\n", day);
-					printf("Remaining days: %d\n", 365 - day);*/
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
