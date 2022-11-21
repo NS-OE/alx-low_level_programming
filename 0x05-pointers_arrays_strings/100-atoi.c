6d74273db84472cc98379a01797c3eb424cbaabe
NS-OE <sonezokoliejike+github@gmail.com>
2022-11-21 14:24:21 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,15 +9,21 @@
  */
 int _atoi(char *s)
 {
-	int i, nt = 0, sign = 1;
+	int i, nt = 0,ptv = 1, ntv = 1, sign;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == '+')
+		{
+			ptv *= 1;
+		}
+		if (s[i] == '-')
+			ntv *= -1;
+		sign = ptv * ntv;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			nt = (nt * 10) + (s[i] - '0');
-			if (s[i - 1] == '-')
-				sign *= -1;
 			if (s[i + 1] > '9' || s[i + 1] < '0')
 				break;
 		}
