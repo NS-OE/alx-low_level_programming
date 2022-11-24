0cfe7d56ccdbc6ab6a3066d0aa7542d14dda78e0
NS-OE <sonezokoliejike+github@gmail.com>
2022-11-23 20:01:17 -0800

    More Pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - prints a no with putchar only
  *
@@ -11,16 +11,16 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n *= -1;
 	}
 
-	if ((n / 10) == 0 && n != 10)
+	if (n == 0)
 		return;
 
-	if (n == 0)
-		_putchar('0');
+	/*if (n == 0)
+		putchar('0');*/
 
 	print_number(n / 10);
-	_putchar((n % 10) + '0');
+	putchar((n % 10) + '0');
 }
