7c69ea3dc647833ef6bc2a425b817261c5ea860a
NS-OE <sonezokoliejike+github@gmail.com>
2023-03-30 17:12:47 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,18 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == s1[j])
+				s[i] = s2[j];
+		}
 	}
 	return (s);
 }
