44e9438cf0bf43192f0240441c3971eed6284a40
NS-OE <sonezokoliejike+github@gmail.com>
2023-04-18 21:46:38 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -45,11 +45,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	return (copy);
 }
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    return (0);
-}
