f2d7bfcc13aee57a660c2c893b0804e69e8764d4
NS-OE <sonezokoliejike+github@gmail.com>
2023-04-18 20:41:59 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,20 +13,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *d;
-	char *nam;
-	char *own;
+	int i, nmlen, ownlen;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+		return (NULL);
+	nmlen = ownlen = 0;
+	while (name[nmlen++])
+		;
+	while (owner[ownlen++])
+		;
+	d->name = malloc(nmlen * sizeof(d->name));
+	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	d->name = nam = name;
+	for(i = 0; i<= nmlen; i++)
+		d->name[i] = name[i];
 	d->age = age;
-	d->owner = own = owner;
-
+	d->owner = malloc(ownlen * sizeof(d->owner));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for(i = 0; i<= ownlen; i++)
+		d->owner[i] = owner[i];
 	return (d);
 }
-
-
