d38c019f30494dcfb5f0036a9b3da4424aa66e3b
NS-OE <sonezokoliejike@gmail.com>
2023-06-07 21:26:32 -0700

    recur palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _strlen - Checks length of string
+ *
+ * @s:  pointer to string
+ * Return: returns 1 or 0 to main.
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (_strlen(s + 1) + 1);
+}
+
+/**
+ * is_drome - checks returns 1 if a string is a palindrome and 0 if not
+ * @s: string being checked
+ * @p: string length
+ * Return: returns int.
+ */
+
+int is_drome(int p, char *s)
+{
+	int d;
+
+	if (*s == '\0')
+		return (1);
+
+	if (*s == s[p - 1])
+		return (is_drome(p - 2, s + 1));
+
+	else
+		return (0);
+}
+/**
+ * is_palindrome - checks returns 1 if a string is a palindrome and 0 if not
+ * @s: string being checked
+ * Return: returns 1 or 0 to main.
+ */
+int is_palindrome(char *s)
+{
+	int p;
+
+	p = _strlen(s);
+	if (*s == s[p - 1])
+		return (is_drome(p - 2, s + 1));
+
+	else
+		return (0);
+}
