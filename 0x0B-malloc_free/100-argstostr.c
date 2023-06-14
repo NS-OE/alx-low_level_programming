f800b3c5a76527c30efa6f5a0c5e621476402d26
NS-OE <sonezokoliejike@gmail.com>
2023-06-13 19:46:54 -0700

    malloc adv

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * argtostr - frees a 2-D grid previously created by your alloc_grid function
+ * argstostr - frees a 2-D grid previously created by your alloc_grid function
  * @ac: array being freed
  * @av: rows of array
  * Return: Always 0.
@@ -10,14 +10,29 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i;
 	char *con;
+	int i, j, k = 0, p = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	con = malloc(sizeof(av) * ac);
-	for (i = 1; i < ac; i++)
-		printf("%s\n", av[i]);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		p += j;
+	}
 
+	con = malloc((sizeof(char) * p) + ac);
+	if (con == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			con[k] = av[i][j];
+			k++;
+		}
+		con[k] = '\n';
+		k++;
+	}
 	return (con);
 }
